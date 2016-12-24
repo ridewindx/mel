fd807e9eb606a0959b94bc187c9d988c1db1f3d2
RideWindX <ridewindx@163.com>
2016-12-24 20:13:21 +0800

    Update routergroup.go

--- routergroup.go
+++ routergroup.go
@@ -1,26 +1,142 @@
 package mel
 
-type RouterGroup struct {
-	basePath string
-	handlers []Handler
-	mel *Mel
+import (
+	"regexp"
+	"strings"
+)
+
+type RouterGroup interface {
+    Group(string, ...Handler) *RouterGroup
+
+	Use(...Handler)
+
+	Handle(string, interface{}, ...Handler)
+	Get(string, interface{}, ...Handler)
+	Post(string, interface{}, ...Handler)
+	Head(string, interface{}, ...Handler)
+	Delete(string, interface{}, ...Handler)
+	Put(string, interface{}, ...Handler)
+	Options(string, interface{}, ...Handler)
+	Trace(string, interface{}, ...Handler)
+	Patch(string, interface{}, ...Handler)
+	Any(string, interface{}, ...Handler)
 }
 
-func (group *RouterGroup) Use(middleware ...Handler) {
-	group.handlers = append(group.handlers, middleware...)
+// routerGroup is used internally to configure router,
+// a routerGroup is associated with a prefix and an array of handlers (middleware)
+type routerGroup struct {
+	basePath string
+	handlers []Handler
+	router *router
 }
+
+var _ RouterGroup = &routerGroup{}
 
 // Group creates a new router group.
 // You should add all the routes that have common middlwares or the same path prefix.
-func (group *RouterGroup) Group(relativePath string, handlers ...Handler) *RouterGroup {
-	return &RouterGroup{
+func (group *routerGroup) Group(relativePath string, handlers ...Handler) *RouterGroup {
+	return &routerGroup{
 		basePath: joinPaths(group.basePath, relativePath),
 		handlers: group.combineHandlers(handlers),
-		mel: group.mel,
+		router: group.router,
 	}
 }
 
-func (group *RouterGroup) combineHandlers(handlers []Handler) []Handler {
+// Use adds middleware to the group.
+func (group *routerGroup) Use(middleware ...Handler) {
+	group.handlers = append(group.handlers, middleware...)
+}
+
+func (group *routerGroup) handle(httpMethod, relativePath string, target interface{}, handlers []Handler) {
+	absolutePath := joinPaths(group.basePath, relativePath)
+    handlers = group.combineHandlers(handlers)
+	group.router.AddRoute(httpMethod, absolutePath, target, handlers...)
+}
+
+// Handle registers a new request handle and middleware with the given path and method.
+// The last handler should be the real handler, the other ones should be middleware
+// that can and should be shared among different routes.
+//
+// This function is intended for bulk loading and to allow the usage of less
+// frequently used, non-standardized or custom methods (e.g. for internal
+// communication with a proxy).
+func (group *routerGroup) Handle(httpMethod, relativePath string, target interface{}, handlers ...Handler) {
+    if matches, err := regexp.MatchString("^[A-Z]+$", httpMethod); !matches || err != nil {
+		panic("HTTP method " + httpMethod + " is invalid")
+	}
+	group.handle(httpMethod, relativePath, target, handlers)
+}
+
+func (group *routerGroup) Get(relativePath string, target interface{}, handlers ...Handler) {
+	group.handle("GET", relativePath, target, handlers)
+}
+
+func (group *routerGroup) Post(relativePath string, target interface{}, handlers ...Handler) {
+	group.handle("POST", relativePath, target, handlers)
+}
+
+func (group *routerGroup) Head(relativePath string, target interface{}, handlers ...Handler) {
+	group.handle("Head", relativePath, target, handlers)
+}
+
+func (group *routerGroup) Delete(relativePath string, target interface{}, handlers ...Handler) {
+	group.handle("DELETE", relativePath, target, handlers)
+}
+
+func (group *routerGroup) Put(relativePath string, target interface{}, handlers ...Handler) {
+	group.handle("PUT", relativePath, target, handlers)
+}
+
+func (group *routerGroup) Options(relativePath string, target interface{}, handlers ...Handler) {
+	group.handle("OPTIONS", relativePath, target, handlers)
+}
+
+func (group *routerGroup) Trace(relativePath string, target interface{}, handlers ...Handler) {
+	group.handle("TRACE", relativePath, target, handlers)
+}
+
+func (group *routerGroup) Patch(relativePath string, target interface{}, handlers ...Handler) {
+	group.handle("PATCH", relativePath, target, handlers)
+}
+
+// Any registers a route that matches all the HTTP methods.
+func (group *routerGroup) Any(relativePath string, target interface{}, handlers ...Handler) {
+	group.Get(relativePath, target, handlers)
+    group.Post(relativePath, target, handlers)
+	group.Head(relativePath, target, handlers)
+	group.Delete(relativePath, target, handlers)
+	group.Put(relativePath, target, handlers)
+	group.Options(relativePath, target, handlers)
+	group.Trace(relativePath, target, handlers)
+	group.Patch(relativePath, target, handlers)
+}
+
+// StaticFile registers a single route in order to server a single file of the local filesystem.
+// router.StaticFile("favicon.ico", "./resources/favicon.ico")
+func (group *routerGroup) StaticFile(relativePath, filePath string) {
+	if strings.Contains(relativePath, ":") || strings.Contains(relativePath, "*") {
+		panic("URL parameters can not be used when serving a static file")
+	}
+
+	handler := func(c *Context) {
+		c.File(filePath)
+	}
+
+	group.Get(relativePath, handler)
+	group.Head(relativePath, handler)
+}
+
+func (group *RouterGroup) StaticDir(relativePath, root string) {
+	if strings.Contains(relativePath, ":") || strings.Contains(relativePath, "*") {
+		panic("URL parameters can not be used when serving a static folder")
+	}
+
+	fs := Dir(root, false)
+
+	absolutePath := joinPaths(group.)
+}
+
+func (group *routerGroup) combineHandlers(handlers []Handler) []Handler {
 	finalSize := len(group.handlers) + len(handlers)
 	if finalSize >= int(abortIndex) {
 		panic("too many handlers")
@@ -30,8 +146,3 @@ func (group *RouterGroup) combineHandlers(handlers []Handler) []Handler {
 	copy(mergedHandlers[len(group.handlers):], handlers)
 	return mergedHandlers
 }
-
-func (group *RouterGroup) calculateAbsolutePath(relativePath string) string {
-	return joinPaths(group.basePath, relativePath)
-}
-
